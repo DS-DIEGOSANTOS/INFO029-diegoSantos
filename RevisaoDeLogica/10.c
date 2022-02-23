0f98a46860d8f7663630c5584f1433c839e86ecf
DIEGO SANTOS DOS SANTOS <84682022+DS-DIEGOSANTOS@users.noreply.github.com>
2022-02-23 20:09:41 -0300

    revisao

--- RevisaoDeLogica/10.c
+++ RevisaoDeLogica/10.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 5
 
-struct pessoa{
-    char nome[30];
-    char sexo[20];
+typedef struct pessoa{
+    char nome[31];
+    char sexo;
     int idade;
-    int cpf;
-};
+    char cpf[16];
+} Pessoa;
 
 
 int main(){
 
-    struct pessoa pessoa[5];
+    Pessoa pessoa[TAM];
 
-    for(int i = 0; i < 5 ;i++ ){
+    for(int i = 0; i < TAM ;i++ ){
         printf("cadastro %d \n", i+1);
 
         printf("digite seu nome:");
-        scanf("%s", & pessoa[i].nome);
-        
+        fgets(pessoa[i].nome,31,stdin);
+
+        size_t ln = strlen(pessoa[i].nome) - 1;
+        if (pessoa[i].nome[ln] == '\n')
+            pessoa[i].nome[ln] = '\0';    
+
         printf("digite seu sexo:");
-        scanf("%s", & pessoa[i].sexo);
+        scanf("%c",& pessoa[i].sexo);
 
         printf("digite sua idade:");
         scanf("%d",& pessoa[i].idade);
 
+        getchar();
+
         printf("digite seu cpf:");
-        scanf("%d",& pessoa[i].cpf);
+        fgets(pessoa[i].cpf,15,stdin);
+        ln = strlen(pessoa[i].nome) - 1;
+        if (pessoa[i].cpf[ln] == '\n')
+            pessoa[i].cpf[ln] = '\0'; 
 
     }
 
@@ -34,9 +44,9 @@ int main(){
  
         printf("cadastro %d \n", i+1);   
         printf("nome: %s\n", pessoa[i].nome);
-        printf("sexo: %s\n", pessoa[i].sexo);
+        printf("sexo: %c\n", pessoa[i].sexo);
         printf("idade: %d\n", pessoa[i].idade);
-        printf("CPF: %d\n", pessoa[i].cpf);
+        printf("CPF: %s\n", pessoa[i].cpf);
         printf("----------------------------------\n");
 
     }
